a947fd96a8504dac6f38ce4937e5d5e326ef5953
Yaroslav Solomianyi <Soman74geeev@gmail.com>
2024-06-14 13:59:15 +0300

    Add database and auth or user module

---
+++ src/user/user.model.ts
@@ -0,0 +1,17 @@
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { Prop } from '@typegoose/typegoose';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface UserModel extends Base {}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export class UserModel extends TimeStamps {
+  @Prop({ unique: true })
+  email: string;
+  @Prop()
+  password: string;
+  @Prop({ default: false })
+  isAdmin?: boolean;
+  @Prop({ default: [] })
+  favorites?: [];
+}
